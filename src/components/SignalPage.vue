c70a3bac849b3b86c5aada36375b6e108c6d9cbe
Melkishar J Delly <56651745+melkdelly@users.noreply.github.com>
2022-08-18 22:54:06 -0400

    added header to file for logout btn

--- src/components/SignalPage.vue
+++ src/components/SignalPage.vue
@@ -1,28 +1,36 @@
-<template> 
-
+<template>
+<MyHeader />
 <table>
-  <tr>
-    <th> Date/Time </th>
-    <th> Signal </th> 
-    <th> Signal Desc. </th>
-    <th> Alarm #</th>
-  </tr>
+  <thead>
+    <tr>
+      <th> Date/Time </th>
+      <th> Signal </th>
+      <th> Signal Desc. </th>
+      <th> Alarm #</th>
+    </tr>
+  </thead>
+  <tbody>
+    <td> </td>
+  </tbody>
 </table>
 </template>
 
 <script>
+import MyHeader from './MyHeader.vue'
+
 export default {
-    name: 'SignalPage',
-    data () {
-      return {
-        siteDate: '',
-        signalCode: '',
-        eventCodeDesc: '',
-        pointDesc: '',
-        alarmNum: 0,
-        xmit: ''
-      }
+  name: 'SignalPage',
+  data () {
+    return {
+      siteDate: '',
+      signalCode: '',
+      eventCodeDesc: '',
+      pointDesc: '',
+      alarmNum: 0,
+      xmit: ''
     }
+  },
+  components: { MyHeader }
 }
 </script>
 
