b3385ade689fcce922025674672408375497030d
Melkishar J Delly <56651745+melkdelly@users.noreply.github.com>
2022-08-20 10:17:28 -0400

    calling API / added methods & mounted object

--- src/components/SignalPage.vue
+++ src/components/SignalPage.vue
@@ -6,7 +6,7 @@
       <th> Date/Time </th>
       <th> Signal </th>
       <th> Signal Desc. </th>
-      <th> Alarm #</th>
+      <th> Alarm # </th>
     </tr>
   </thead>
   <tbody>
@@ -17,20 +17,42 @@
 
 <script>
 import MyHeader from './MyHeader.vue'
+import axios from 'axios'
 
 export default {
   name: 'SignalPage',
   data () {
     return {
-      siteDate: '',
-      signalCode: '',
-      eventCodeDesc: '',
-      pointDesc: '',
-      alarmNum: 0,
-      xmit: ''
+      signalsURL: 'https://grasperapi.azurewebsites.net/api/v1/Users/Signals',
+      signals: [],
+      signalsData: {
+        per_page: 10,
+        page: 1
+      },
+      pagination: {
+        nextPage: '',
+        previousPage: '',
+        totalRecords: 0,
+        currentPage: 0,
+        pageSize: 0,
+        numPages: 0
+      }
     }
   },
-  components: { MyHeader }
+  components: { MyHeader },
+  methods: {
+    getSignals () {
+      axios.get(this.signalsURL, { params: this.signalsData })
+        .then((response) => { this.signals = response.data })
+        .catch((error) => { console.log(error) })
+    }
+  },
+  mounted () {
+    const user = localStorage.getItem('userInfo')
+    if (!user) {
+      this.$router.push({ name: 'LogIn' })
+    }
+  }
 }
 </script>
 
